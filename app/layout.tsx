be49ccfe64ab7c203a1f0265f2aaa3a8f3789c69
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-05-16 18:14:42 +0530

    rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "FinCalc",
+  title: "FinMinCalc",
   description: "Excel replacement for financial modeling",
 };
 
